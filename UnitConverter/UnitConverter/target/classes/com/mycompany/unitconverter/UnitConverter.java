696d72f96cb1d4f1002e24aac12a0237c1768573
XYZ <XYZ@example.com>
2023-12-21 03:18:18 +0530

    first commit

---
+++ UnitConverter/UnitConverter/target/classes/com/mycompany/unitconverter/UnitConverter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unitconverter;
+
+/**
+ *
+ * @author ShadowFire
+ */
+public class UnitConverter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
